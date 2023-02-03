8a06e72b58ea2c85023c6705726ee93b2677a12e
Hasdrubal <ronaldolsthoorn1@gmail.com>
2023-02-03 18:47:48 +0100

    continue

--- src/main.rs
+++ src/main.rs
@@ -14,14 +14,14 @@ struct Graph {
 #[derive(Clone, Debug)]
 struct Vertex {
     id : String,
-    pressure: i32
+    pressure: usize
 }
 
 struct Path {
     from: u64,
     to: u64,
     len: usize,
-    pressure: i32,
+    pressure: usize,
     vertices :Vec<u64>
 }
 
@@ -41,18 +41,18 @@ fn calculate_shortest_path(graph: &Graph, start: u64, end: u64) -> Vec<u64> {
 
     let mut dist: HashMap<u64, usize> = HashMap::new();
     let mut prev: HashMap<u64, u64> = HashMap::new();
-    let mut Q: HashSet<u64> = HashSet::new();
+    let mut q: HashSet<u64> = HashSet::new();
 
-    for (h, v) in graph.vertices.iter(){
+    for (h, _v) in graph.vertices.iter(){
         dist.insert(*h, usize::MAX);
-        Q.insert(*h);
+        q.insert(*h);
     }
 
     dist.insert(start, 0);
 
-    while !Q.is_empty() {
+    while !q.is_empty() {
 
-        let u = *Q.iter().min_by(|&h1, &h2| dist[h1].cmp(&dist[h2])).unwrap();
+        let u = *q.iter().min_by(|&h1, &h2| dist[h1].cmp(&dist[h2])).unwrap();
 
         if u == end {
 
@@ -67,9 +67,9 @@ fn calculate_shortest_path(graph: &Graph, start: u64, end: u64) -> Vec<u64> {
             return path;
         }
 
-        Q.remove(&u);
+        q.remove(&u);
 
-        for neighbour in graph.adjacencies[&u].iter().filter(|h| Q.contains(h)) {
+        for neighbour in graph.adjacencies[&u].iter().filter(|h| q.contains(h)) {
             let alt = dist[&u] + 1;
 
             if alt < dist[neighbour] {
@@ -82,21 +82,66 @@ fn calculate_shortest_path(graph: &Graph, start: u64, end: u64) -> Vec<u64> {
     return vec![];
 }
 
-fn simulate(graph: &Graph, loosePumps: Vec<u64>, mut paths: HashMap<u64, Vec<(u64, Path)>>) {
+fn simulate(graph: &Graph, loose_pumps: Vec<u64>, mut paths: HashMap<u64, Vec<(u64, Path)>>) {
 
-    let mut time = 0;
 
-    for (from_h, p) in paths.iter_mut() {
-        p.sort_by();
+
+    for (_from_h, p) in paths.iter_mut() {
+        p.sort_by(|a, b|
+            (a.1.pressure - a.1.len).cmp(&(b.1.pressure - b.1.len)));
     }
 
+    let mut time = 0;
+    let mut pressure_open = 0;
+    let mut pressure_released = 0;
+
+    let start_node_h : u64 = 0;
+
+
+    let mut q = VecDeque::new();
+    q.push_front(start);
+
+    let mut passed_nodes: HashSet<u64> = HashSet::new();
+
+    while !q.is_empty() {
+
+        let current_node_h = q.pop_front().unwrap();
+        let current_node: Vertex = graph.vertices[current_node_h];
+        passed_nodes.insert(current_node_h);
+
+        if current_node.pressure > 0 {
+            time += 1;
+            pressure_released += pressure_open;
+            pressure_open += current_node.pressure;
+        }
+
+
+        let to_paths: &Vec<(u64, Path)> = paths[current_node_h];
+        let children = to_paths.iter().filter(
+            |&t| passed_nodes.contains(&t.0) && t.1.len < (30 - time)
+        ).collect();
+
+
+        if children.is_empty {
+            println!("found: {}", pressure_released + (30 - time) * pressure_open);
+            passed_nodes.remove(current_node_h);
+
+        } else {
+            for c in children{
+                q.push_front(c);
+            }
+        }
+
+
+
+
+
+    }
 
 }
 
 
 fn main() {
-
-    let reader = BufReader::new(File::open("input.txt").unwrap());
 
     let mut graph: Graph = Graph {
         vertices: HashMap::new(),
@@ -111,7 +156,7 @@ fn main() {
 
         let splits: Vec<&str> = line.split_whitespace().collect();
 
-        let v = Vertex{id: splits[1].to_string(), pressure: *&splits[4][5..splits[4].len()-1].parse::<i32>().unwrap() };
+        let v = Vertex{id: splits[1].to_string(), pressure: *&splits[4][5..splits[4].len()-1].parse::<usize>().unwrap() };
         graph.vertices.insert(calculate_hash(&v), v);
     }
 
@@ -131,29 +176,33 @@ fn main() {
             hashes);
     }
 
-    let mut loosePumps: Vec<u64> = Vec::new();
+    let mut loose_pumps: Vec<u64> = Vec::new();
 
     for (h, v) in graph.vertices.iter() {
         if v.pressure > 0 {
-            loosePumps.push(*h);
+            loose_pumps.push(*h);
         }
     }
 
     let mut paths: HashMap<u64, Vec<(u64, Path)>> = HashMap::new();
 
-    for from in loosePumps.iter() {
+    for from in loose_pumps.iter() {
 
-        let mut toPaths = Vec::new();
-        for to in loosePumps.iter() {
+        let mut to_paths = Vec::new();
+        for to in loose_pumps.iter() {
             if from != to {
+
+                let vs = calculate_shortest_path(&graph, *from, *to);
                 let p = Path{
-                    from:*from, to: *to, pressure: graph.vertices[to].pressure,
-                    vertices: calculate_shortest_path(&graph, *from, *to)
+                    from:*from, to: *to,
+                    len: vs.len(),
+                    pressure: graph.vertices[to].pressure,
+                    vertices: vs
                 };
-                toPaths.push((*to, p));
+                to_paths.push((*to, p));
             }
         }
 
-        paths.insert(*from, toPaths);
+        paths.insert(*from, to_paths);
     }
 }
