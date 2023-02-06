593f454debb6eb522cc6afd9f14de2628727243e
Ronald Olsthoorn <ronaldolsthoorn1@gmail.com>
2023-02-06 13:45:47 +0100

    example with good result

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,10 @@
+use std::cmp::Ordering;
 use std::collections::{HashMap, HashSet};
 use std::collections::hash_map::DefaultHasher;
 use std::collections::vec_deque::VecDeque;
 use std::fs::File;
 use std::hash::{Hash, Hasher};
 use std::io::{BufReader, BufRead};
-use std::{thread, time};
 
 struct Graph {
     vertices: HashMap<u64, Vertex>,
@@ -14,15 +14,29 @@ struct Graph {
 #[derive(Clone, Debug)]
 struct Vertex {
     id : String,
-    pressure: usize
+    pressure: i32
 }
 
 struct Path {
     from: u64,
     to: u64,
-    len: usize,
-    pressure: usize,
+    dist: i32,
+    pressure: i32,
     vertices :Vec<u64>
+}
+
+#[derive(Clone, Debug)]
+struct WalkState {
+    path: Vec<u64>,
+    time: i32,
+    pressure_released: i32,
+    pressure_open: i32
+}
+
+#[derive(Clone, Debug)]
+struct WalkTransition {
+    new_node: u64,
+    dist: i32
 }
 
 impl Hash for Vertex {
@@ -36,6 +50,8 @@ fn calculate_hash<T: Hash>(t: &T) -> u64 {
     t.hash(&mut s);
     s.finish()
 }
+
+const MAX_TIME: i32 = 30;
 
 fn calculate_shortest_path(graph: &Graph, start: u64, end: u64) -> Vec<u64> {
 
@@ -82,64 +98,73 @@ fn calculate_shortest_path(graph: &Graph, start: u64, end: u64) -> Vec<u64> {
     return vec![];
 }
 
-fn simulate(graph: &Graph, loose_pumps: Vec<u64>, mut paths: HashMap<u64, Vec<(u64, Path)>>) {
+fn simulate(graph: &Graph, paths: &mut HashMap<u64, Vec<(u64, Path)>>, start_node_h: u64) {
 
+    let origin_state = WalkState{ 
+        path: vec![], time: 0, pressure_released: 0, pressure_open: 0 };
 
+    let mut q: VecDeque<(WalkState, WalkTransition)> = VecDeque::new();
+    q.push_front((origin_state, WalkTransition{new_node: start_node_h, dist: 0}));
 
-    for (_from_h, p) in paths.iter_mut() {
-        p.sort_by(|a, b|
-            (a.1.pressure - a.1.len).cmp(&(b.1.pressure - b.1.len)));
-    }
+    let mut results: Vec<(i32, Vec<u64>)> = Vec::new();
 
-    let mut time = 0;
-    let mut pressure_open = 0;
-    let mut pressure_released = 0;
-
-    let start_node_h : u64 = 0;
-
-
-    let mut q = VecDeque::new();
-    q.push_front(start);
-
-    let mut passed_nodes: HashSet<u64> = HashSet::new();
 
     while !q.is_empty() {
 
-        let current_node_h = q.pop_front().unwrap();
-        let current_node: Vertex = graph.vertices[current_node_h];
-        passed_nodes.insert(current_node_h);
+        let (old_state, transition) = q.pop_front().unwrap();
+        let current_node_h = transition.new_node;
+        let current_node: &Vertex = &graph.vertices[&current_node_h];
+        let current_state = tick(&old_state, &transition, &current_node);
 
-        if current_node.pressure > 0 {
-            time += 1;
-            pressure_released += pressure_open;
-            pressure_open += current_node.pressure;
-        }
+        paths.get_mut(&transition.new_node).unwrap().sort_by(|a, b|
+            compare_paths(&a.1, &b.1, &current_state.time));
 
+        let children: Vec<(u64, &Path)> = (&paths[&current_node_h]).iter().filter_map(
+            |a| if current_state.path.contains(&a.0) || a.1.dist > MAX_TIME - current_state.time {None} else {Some((a.0, &a.1))}).collect();
 
-        let to_paths: &Vec<(u64, Path)> = paths[current_node_h];
-        let children = to_paths.iter().filter(
-            |&t| passed_nodes.contains(&t.0) && t.1.len < (30 - time)
-        ).collect();
-
-
-        if children.is_empty {
-            println!("found: {}", pressure_released + (30 - time) * pressure_open);
-            passed_nodes.remove(current_node_h);
-
+        if children.is_empty() {
+            let end_result = current_state.pressure_released + (MAX_TIME - current_state.time) * current_state.pressure_open;
+            println!("Path: {:?}", current_state.path);
+            println!("found: {}", end_result);
+            results.push((end_result, current_state.path));
         } else {
-            for c in children{
-                q.push_front(c);
+            for c in children.iter() {
+                q.push_front((current_state.clone(), WalkTransition{ new_node: c.0, dist: c.1.dist }));
             }
         }
-
-
-
-
-
     }
+
+    results.sort_by(|a, b| a.0.cmp(&b.0));
+
+    println!("best_result: {} worst result: {}", results[0].0, results[results.len()-1].0);
 
 }
 
+
+fn tick(state: &WalkState, transition: &WalkTransition, new_node: &Vertex) -> WalkState {
+    let mut w = state.clone();
+    w.path.push(transition.new_node);
+    w.time += transition.dist;
+    w.pressure_released += w.pressure_open * transition.dist;
+
+    if new_node.pressure > 0 {
+        w.time += 1;
+        w.pressure_released += w.pressure_open;
+        w.pressure_open += new_node.pressure;
+    }
+
+    w
+}
+
+
+fn compare_paths(a: &Path, b: &Path, time: &i32) -> Ordering {
+
+    let a_res = (MAX_TIME - time - 1 - a.dist) * a.pressure;
+    let b_res = (MAX_TIME - time - 1 - b.dist) * b.pressure;
+
+    let res = a_res.cmp(&b_res);
+    return res;
+}
 
 fn main() {
 
@@ -148,19 +173,23 @@ fn main() {
         adjacencies: HashMap::new(),
     };
 
-    let mut start_o: Option<Vertex> = None;
+    let mut start_o: Option<u64> = None;
 
-    let reader = BufReader::new(File::open("input.txt").unwrap());
+    let reader = BufReader::new(File::open("test_input.txt").unwrap());
 
     for line in reader.lines().map(|l| l.unwrap()) {
 
         let splits: Vec<&str> = line.split_whitespace().collect();
 
-        let v = Vertex{id: splits[1].to_string(), pressure: *&splits[4][5..splits[4].len()-1].parse::<usize>().unwrap() };
+        let v = Vertex{id: splits[1].to_string(), pressure: *&splits[4][5..splits[4].len()-1].parse::<i32>().unwrap() };
+        if v.id == "AA" {
+            start_o = Some(calculate_hash(&v));
+        }
+
         graph.vertices.insert(calculate_hash(&v), v);
     }
 
-    let reader = BufReader::new(File::open("input.txt").unwrap());
+    let reader = BufReader::new(File::open("test_input.txt").unwrap());
 
     for line in reader.lines().map(|l| l.unwrap()) {
 
@@ -177,6 +206,7 @@ fn main() {
     }
 
     let mut loose_pumps: Vec<u64> = Vec::new();
+    loose_pumps.push(start_o.unwrap());
 
     for (h, v) in graph.vertices.iter() {
         if v.pressure > 0 {
@@ -195,7 +225,7 @@ fn main() {
                 let vs = calculate_shortest_path(&graph, *from, *to);
                 let p = Path{
                     from:*from, to: *to,
-                    len: vs.len(),
+                    dist: vs.len() as i32,
                     pressure: graph.vertices[to].pressure,
                     vertices: vs
                 };
@@ -205,4 +235,6 @@ fn main() {
 
         paths.insert(*from, to_paths);
     }
+
+    simulate(&graph, &mut paths, start_o.unwrap());
 }
